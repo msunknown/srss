64df29c313b11516b9212660544bb2f363eac6af
msunknown <srsssonu2gmail.com>
2025-05-11 18:49:41 +0530

    3rd

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int numa = 20;
+        int number = 20;
         int numb = 50;
-        int sum = numa + numb;
+        int sum = number + numb;
 
         System.out.println("Sum: " + sum);
     }
