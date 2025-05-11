3898c06d78aedd2eec356a33dec8b63606ca98cc
msunknown <srsssonu2gmail.com>
2025-05-11 15:17:22 +0530

    1st cmt

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 20;
+        int num2 = 20;
+        int sum = num1 + num2;
+
+        System.out.println("Sum: " + sum);
+    }
+    
+}
